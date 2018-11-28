afb690d34d3910862f979bd69676b106e5327a08
jomorro <morrowjoe0@gmail.com>
2018-11-28 12:06:32 -0500

    Updates

---
+++ precache-manifest.7e542a4d125d58a19695c885bbd75301.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todos-part-1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6ced8597f93d1e0b2f2c",
+    "url": "/todos-part-1/static/js/runtime~main.6ced8597.js"
+  },
+  {
+    "revision": "4a7b8d6f4037e65de8f4",
+    "url": "/todos-part-1/static/js/main.4a7b8d6f.chunk.js"
+  },
+  {
+    "revision": "d855c10c80aa95827f2a",
+    "url": "/todos-part-1/static/js/1.d855c10c.chunk.js"
+  },
+  {
+    "revision": "4a7b8d6f4037e65de8f4",
+    "url": "/todos-part-1/static/css/main.4c7a143e.chunk.css"
+  },
+  {
+    "revision": "5203e51ecd9961f1cc262925f857410b",
+    "url": "/todos-part-1/index.html"
+  }
+];
